d17e76b02d9e8721432cb279770361a290f21bb6
yangchongduo <yangchongduo@le.com>
2017-02-07 18:08:47 +0800

    xx

---
+++ test.js
@@ -0,0 +1,19 @@
+/*const data=Object.create()
+console.log(data)*/
+/*class Point{
+    toString(){
+        console.log('PoinT的方法')
+    }
+}
+class ColorPoint extends Point {
+    constructor(x, y, color) {
+        super(x, y); // 调用父类的constructor(x, y)
+        this.color = color;
+    }
+
+    toString() {
+        return this.color + ' ' + super.toString(); // 调用父类的toString()
+    }
+}
+var ccc=new ColorPoint('xxx')
+ccc.toString()*/
